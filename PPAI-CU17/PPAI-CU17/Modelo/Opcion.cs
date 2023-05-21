d2c916ec4050051095cebe6862a0477248ec0e92
paulachudnosky <63914690+paulachudnosky@users.noreply.github.com>
2023-05-21 15:39:27 -0300

    Update Opcion.cs

--- PPAI-CU17/PPAI-CU17/Modelo/Opcion.cs
+++ PPAI-CU17/PPAI-CU17/Modelo/Opcion.cs
@@ -3,17 +3,57 @@
 namespace PPAI_CU17.Modelo;
 	public class Opcion
 	{
-		public Opcion()
-		{
-		}
+	// atributos para opcion
+	private string audioMensajeSubopcion;
+        private string mensajeSubopcion;
+        private string nombreSubop;
+        private int nroOrdenSubop;
+        private List<Subopcion> subopciones;
+        public Categoria categoriaPadre;
+      
+      // constructor para opcion
+        public Opcion(string audioMensajeSubopciones, string mensajeSubopciones, string nombre, int nroOrden)
+        {
+            this.audioMensajeSubopcion = audioMensajeSubopciones;
+            this.mensajeSubopcion = mensajeSubopciones;
+            this.nombreSubop = nombre;
+            this.nroOrdenSubop = nroOrden;
+            subopciones = new List<Subopcion>();
+            categoriaPadre = null;
+        }
 
-		public string getDatos()
-		{
-			return "datos de opcion";
-		}
+        // Agregamos subopciones a la opcion
+        public void AgregarSubopcion(Subopcion _subopcion)
+        {
+            _subopcion.opcionPadre = this;
+            subopciones.Add(_subopcion);
+        }
 
-		public void getCategoria(Categoria categoria)
-		{
-			categoria.getDatos();
-		}
+        // metodos get para opcion
+
+        public string getAudioMensajeSubopcion()
+        {
+            return audioMensajeSubopcion;
+        }
+
+        public string getMensajeSubopcion()
+        {
+            return mensajeSubopcion;
+        }
+
+        public string getNombreSubop()
+        {
+            return nombreSubop;
+        }
+
+        public int getNroOrdenSubop()
+        {
+            return nroOrdenSubop;
+        }
+
+        // get categoria
+        public Categoria getCategoria()
+        {
+            return categoriaPadre;
+        }
 	}
