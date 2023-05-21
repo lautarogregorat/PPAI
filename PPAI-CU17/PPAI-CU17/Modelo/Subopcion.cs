5afe23ffacfe721d79c858497b4e7efdc71d122d
paulachudnosky <63914690+paulachudnosky@users.noreply.github.com>
2023-05-21 15:37:43 -0300

    Update Subopcion.cs

--- PPAI-CU17/PPAI-CU17/Modelo/Subopcion.cs
+++ PPAI-CU17/PPAI-CU17/Modelo/Subopcion.cs
@@ -8,8 +8,39 @@ namespace PPAI_CU17.Modelo
 {
     public class Subopcion
     {
-        public Subopcion() { }
-        
+        // atributos
+        private string nombre;
+        private int nroOrden;
+        public Opcion opcionPadre;
+
+        // constructor para subopcion
+        public Subopcion(string nombre, int nroOrden)
+        {
+            this.nombre = nombre;
+            this.nroOrden = nroOrden;
+            opcionPadre = null;
+        }
+
+        // metodos get para subopcion
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public int getNroOrden()
+        {
+            return nroOrden;
+        }
+
+       public Opcion  getOpcion()
+        {
+            return opcionPadre;
+        }
+
+        /* public object getValidaciones()
+        {
+            return validacion (pero todavia no esta programada la parte de las validaciones)
+        } */
     }
 
 
