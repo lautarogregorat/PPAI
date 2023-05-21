a9fa4203a28ae210340c81905613414a82ad147a
paulachudnosky <63914690+paulachudnosky@users.noreply.github.com>
2023-05-21 15:40:34 -0300

    Update Categoria.cs

--- PPAI-CU17/PPAI-CU17/Modelo/Categoria.cs
+++ PPAI-CU17/PPAI-CU17/Modelo/Categoria.cs
@@ -8,12 +8,49 @@ namespace PPAI_CU17.Modelo
 {
     public class Categoria
     {
-        public Categoria()
-        { 
-        }
-        public string getDatos()
+        // atributos para categoria
+        private string audioMensajeOpcion; 
+        private string mensajeOpcion;
+        private string nombreOp;
+        private int nroOrdenOp;
+        private List<Opcion> opciones;
+
+        // constructor Categoria
+        public Categoria(string audioMensajeOpcion, string mensajeOpcion, string nombre, int nroOrden)
         {
-            return "datos de categoria";
+            this.audioMensajeOpcion = audioMensajeOpcion;
+            this.mensajeOpcion = mensajeOpcion;
+            this.nombreOp = nombre;
+            this.nroOrdenOp = nroOrden;
+            opciones = new List<Opcion>();
+        }
+
+        // Agregamos opciones a la categoria
+        public void AgregarOpcion(Opcion _opcion)
+        {
+            _opcion.categoriaPadre = this;
+            opciones.Add(_opcion);
+        }
+
+        // metodos get para categoria
+        public string getAudioMensajeOpcion()
+        {
+            return audioMensajeOpcion;
+        }
+
+        public string getMensajeOpcion()
+        {
+            return mensajeOpcion;
+        }
+
+        public string getNombreOp()
+        {
+            return nombreOp;
+        }
+
+        public int getNroOrdenOp()
+        {
+            return nroOrdenOp;
         }
     }
 }
