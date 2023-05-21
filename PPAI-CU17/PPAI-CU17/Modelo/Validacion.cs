e73bcd9c902ed1044916d3bb98c6ad5e6e640d37
paulachudnosky <63914690+paulachudnosky@users.noreply.github.com>
2023-05-21 15:53:46 -0300

    Update Validacion.cs

--- PPAI-CU17/PPAI-CU17/Modelo/Validacion.cs
+++ PPAI-CU17/PPAI-CU17/Modelo/Validacion.cs
@@ -8,5 +8,25 @@ namespace PPAI_CU17.Modelo
 {
     public class Validacion
     {
-    
+        // atributos de validacion
+        private string audioMensajeValidacion;
+        private string nombreValidacion;
+        
+        // constructor de validacion
+        public Validacion(string audioMensajeValidacion, string nombreValidacion)
+        {
+            this.audioMensajeValidacion = audioMensajeValidacion;
+            this.nombreValidacion = nombreValidacion;
+        }
+        
+        // metodos get para validacion
+        public string getAudioMensajeValidacion()
+        {
+            return audioMensajeValidacion;
+        }
+        
+        public string nombreValidacion()
+        {
+            return nombreValidacion;
+        }
     }
